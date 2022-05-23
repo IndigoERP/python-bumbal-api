e236f0004d6be08279a4354ae87bda0e8df1990b
karan@dreambits.in <admin@dreambits.in>
2022-05-23 15:44:12 +0530

    Api release

--- git_push.sh
+++ git_push.sh
@@ -8,7 +8,7 @@ git_repo_id=$2
 release_note=$3
 
 if [ "$git_user_id" = "" ]; then
-    git_user_id="karan-indigoerp"
+    git_user_id="IndigoERP"
     echo "[INFO] No command line input provided. Set \$git_user_id to $git_user_id"
 fi
 
